cf3f9a5edcea0c4844dafdafd45e58449ea1f16b
Isaac Shoman <isaac.shoman@gmail.com>
2018-07-13 09:40:42 -0700

    Create react app playground to gain familiarity

---
+++ playground/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+console.log(BrowserRouter);
+
+function App() {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
+
+export default App;
